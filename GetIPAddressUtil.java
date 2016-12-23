bb1803bdd9c33b91ad46d10e0775bd751144b21d
Real.mo <czb166@qq.com>
2016-12-23 23:51:35 +0800

    获得WIFI IP地址或者手机网络IP工具类
    获得WIFI IP地址或者手机网络IP工具类

---
+++ GetIPAddressUtil.java
@@ -0,0 +1,53 @@
+package com.momo.mvp_demo;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * Created by RealMo on 2016-12-23.
+ */
+public class GetIPAddressUtil {
+
+    public static String getWifiIP(Context context) {
+        String ip = null;
+        WifiManager wifiManager = (WifiManager) context
+                .getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager.isWifiEnabled()) {
+            //需要在Manifest声明权限
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            int i = wifiInfo.getIpAddress();
+            ip = (i & 0xFF) + "." + ((i >> 8) & 0xFF) + "." + ((i >> 16) & 0xFF)
+                    + "." + (i >> 24 & 0xFF);
+        }
+        return ip;
+    }
+
+    public static String getMobileIP() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface
+                    .getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf
+                        .getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("哎呀，出错了...", ex.toString());
+        }
+        return null;
+    }
+}
+
+
+
