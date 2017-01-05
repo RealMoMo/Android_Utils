6f78464d8a2e46b3a25a0e98924790545c7cab9a
Real.mo <czb166@qq.com>
2017-01-05 23:49:14 +0800

    Cookie持久化工具类
    Cookie持久化工具类

---
+++ cookie持久化工具类/okHttp Cookie/CookieManger.java
@@ -0,0 +1,43 @@
+package oa.qianfeng.com.oa.utils;
+
+import android.content.Context;
+
+import java.util.List;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+
+public class CookieManger implements CookieJar {
+
+
+    public static String APP_PLATFORM = "app-platform";
+
+
+    private static Context mContext;
+
+    private static PersistentCookieStore cookieStore;
+
+    public CookieManger(Context context) {
+        mContext = context;
+        if (cookieStore == null) {
+            cookieStore = new PersistentCookieStore(mContext);
+        }
+
+    }
+
+    @Override
+    public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+        if (cookies != null && cookies.size() > 0) {
+            for (Cookie item : cookies) {
+                cookieStore.add(url, item);
+            }
+        }
+    }
+
+    @Override
+    public List<Cookie> loadForRequest(HttpUrl url) {
+        List<Cookie> cookies = cookieStore.get(url);
+        return cookies;
+    }
+}
