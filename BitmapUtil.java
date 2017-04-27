effe53f5036b69bead49a88cb870236f2e9f0d9e
Real.mo <czb166@qq.com>
2017-04-27 21:04:26 +0800

    Bitmap工具类
    Bitmap工具类

--- BitmapUtil.java
+++ BitmapUtil.java
@@ -9,6 +9,14 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Bitmap.Config;
+import android.graphics.PorterDuff.Mode;
+import android.net.Uri;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  *  realmo
@@ -141,5 +149,55 @@ public class BitmapUtil {
         //第二次采样,返回解析的图片
         return BitmapFactory.decodeResource(res,id,ops);
     }
+	
+		/**
+	 * Save image to the SD card
+	 * 
+	 * @param photoBitmap
+	 * @param photoName
+	 * @param path
+	 */
+	public static String savePhoto(Bitmap photoBitmap, String path,
+								   String photoName) {
+		String localPath = null;
+		if (android.os.Environment.getExternalStorageState().equals(
+				android.os.Environment.MEDIA_MOUNTED)) {
+			File dir = new File(path);
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+
+			File photoFile = new File(path, photoName + ".png");
+			FileOutputStream fileOutputStream = null;
+			try {
+				fileOutputStream = new FileOutputStream(photoFile);
+				if (photoBitmap != null) {
+					if (photoBitmap.compress(Bitmap.CompressFormat.PNG, 100,
+							fileOutputStream)) { // ת�����
+						localPath = photoFile.getPath();
+						fileOutputStream.flush();
+					}
+				}
+			} catch (FileNotFoundException e) {
+				photoFile.delete();
+				localPath = null;
+				e.printStackTrace();
+			} catch (IOException e) {
+				photoFile.delete();
+				localPath = null;
+				e.printStackTrace();
+			} finally {
+				try {
+					if (fileOutputStream != null) {
+						fileOutputStream.close();
+						fileOutputStream = null;
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return localPath;
+	}
 
 }
