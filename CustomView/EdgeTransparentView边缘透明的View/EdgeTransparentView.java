2562ab266d2cf450d9a3e2243ac65c789cd44402
Real Mo <czb166@qq.com>
2018-01-25 23:08:00 +0800

    EdgeTransparentView
    EdgeTransparentView

---
+++ CustomView/EdgeTransparentView边缘透明的View/EdgeTransparentView.java
@@ -0,0 +1,130 @@
+package com.realmo.view;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.FrameLayout;
+
+
+/**
+ * @author Realmo
+ * @version 1.0.0
+ * @name EdgeTransparentView
+ * @email momo.weiye@gmail.com
+ * @time 2018/1/22 10:04
+ * @describe   任意View边沿渐变透明  嵌套可滚动的布局（Scrollview、NestedScrollView）更佳。
+ */
+public class EdgeTransparentView extends FrameLayout {
+    private Paint mPaint;
+    private int position;
+    private float drawSize;
+
+    private int topMask = 0x01;
+    private int bottomMask = topMask << 1;   //2
+    private int leftMask = topMask << 2;    //4
+    private int rightMask = topMask << 3;   //8
+
+    private int mWidth;
+    private int mHeight;
+
+    public EdgeTransparentView(Context context) {
+        this(context, null);
+    }
+
+    public EdgeTransparentView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public EdgeTransparentView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+
+
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.EdgeTransparentView);
+        position = typedArray.getInt(R.styleable.EdgeTransparentView_edge_position, 0);
+        drawSize = typedArray.getDimension(R.styleable.EdgeTransparentView_edge_width,
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, context.getResources().getDisplayMetrics()));
+
+
+        typedArray.recycle();
+    }
+
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        initShader();
+        mWidth = getWidth();
+        mHeight = getHeight();
+    }
+
+    //渐变颜色
+    private int[] mGradientColors = {0xffffffff, 0x00000000};
+    //渐变位置
+    private float[] mGradientPosition = new float[]{0, 1};
+
+    private void initShader() {
+        mPaint.setShader(new LinearGradient(0, 0, 0, drawSize, mGradientColors, mGradientPosition, Shader.TileMode.CLAMP));
+    }
+
+    @Override
+    protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
+        int layerSave = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        boolean drawChild = super.drawChild(canvas, child, drawingTime);
+        if (position == 0 || (position & topMask) != 0) {
+            canvas.drawRect(0, 0, mWidth, drawSize, mPaint);
+        }
+
+        if (position == 0 || (position & bottomMask) != 0) {
+            int save = canvas.save();
+            canvas.rotate(180, mWidth / 2f, mHeight / 2f);
+            canvas.drawRect(0, 0, mWidth, drawSize, mPaint);
+            canvas.restoreToCount(save);
+        }
+
+        float offset = (mHeight - mWidth) / 2f;
+        if (position == 0 || (position & leftMask) != 0) {
+            int saveCount = canvas.save();
+            canvas.rotate(270, mWidth / 2f, mHeight / 2f);
+            canvas.translate(0, offset);
+            canvas.drawRect(0 - offset, 0, mWidth + offset, drawSize, mPaint);
+            canvas.restoreToCount(saveCount);
+        }
+
+        if (position == 0 || (position & rightMask) != 0) {
+            int saveCount = canvas.save();
+            canvas.rotate(90, mWidth / 2f, mHeight / 2f);
+            canvas.translate(0, offset);
+            canvas.drawRect(0 - offset, 0, mWidth + offset, drawSize, mPaint);
+            canvas.restoreToCount(saveCount);
+        }
+
+        canvas.restoreToCount(layerSave);
+        return drawChild;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
