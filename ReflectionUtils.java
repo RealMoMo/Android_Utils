5e8beda0f3a9f777943df35339a277925c8b07f3
炜烨 <czb166@qq.com>
2019-06-03 09:17:04 +0800

    updated utils
    added ziputil & updated reflectionutils

--- ReflectionUtils.java
+++ ReflectionUtils.java
@@ -134,5 +134,62 @@ public class ReflectionUtils {
         }
         Log.e("reflect", "set field " + fieldName + " not found in " + obj.getClass().getName());
     }
+	
+	
+	    /**
+     * 反射获得 指定对象(当前-》父类-》父类...)中的 成员属性
+     * @param instance
+     * @param name
+     * @return
+     * @throws NoSuchFieldException
+     */
+    public static Field findField(Object instance, String name) throws NoSuchFieldException {
+        Class clazz = instance.getClass();
+        //反射获得
+        while (clazz != null) {
+            try {
+                Field field = clazz.getDeclaredField(name);
+                //如果无法访问 设置为可访问
+                if (!field.isAccessible()) {
+                    field.setAccessible(true);
+                }
+                return field;
+            } catch (NoSuchFieldException e) {
+                //如果找不到往父类找
+                clazz = clazz.getSuperclass();
+            }
+        }
+        throw new NoSuchFieldException("Field " + name + " not found in " + instance.getClass());
+    }
+
+
+    /**
+     * 反射获得 指定对象(当前-》父类-》父类...)中的 函数
+     * @param instance
+     * @param name
+     * @param parameterTypes
+     * @return
+     * @throws NoSuchMethodException
+     */
+    public static Method findMethod(Object instance, String name, Class... parameterTypes)
+            throws NoSuchMethodException {
+        Class clazz = instance.getClass();
+        while (clazz != null) {
+            try {
+                Method method = clazz.getDeclaredMethod(name, parameterTypes);
+                if (!method.isAccessible()) {
+                    method.setAccessible(true);
+                }
+                return method;
+            } catch (NoSuchMethodException e) {
+                //如果找不到往父类找
+                clazz = clazz.getSuperclass();
+            }
+        }
+        throw new NoSuchMethodException("Method " + name + " with parameters " + Arrays.asList
+                (parameterTypes) + " not found in " + instance.getClass());
+    }
+	
+	
 }
 
